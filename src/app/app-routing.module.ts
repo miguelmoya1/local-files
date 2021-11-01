7b533c1e75eb03186a0f7fb8abb3b7b1eb762461
Miguelmo <miguelmoyaortega@gmail.com>
2021-11-01 23:33:46 +0100

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'folder', pathMatch: 'full' },
   {
     path: 'folder',
     loadChildren: () =>
       import('./folder/folder.module').then((m) => m.FolderModule),
   },
-  { path: '**', redirectTo: 'home' },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./landing/landing.module').then((m) => m.LandingModule),
+  },
+  { path: '**', redirectTo: 'folder' },
 ];
 
 @NgModule({
